e9fbb482fb6d39620dec02342bbf9a60aab2598f
Jordan Estorba <jordaneilestorba@gmail.com>
2024-09-16 12:41:34 +1000

    Remove global jsdom

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,34 +2,8 @@
  * @vitest-environment jsdom
  */
 
-import { beforeEach, expect, test } from 'vitest'
-import { render } from './main';
+import { beforeEach, describe, expect, test } from 'vitest';
 
-let container: HTMLDivElement;
-
-function mount() {
-    const div = document.createElement('div');
-    render(div);
-    return div;
-}
-
-beforeEach(() => {
-    container = mount();
-})
-
-test('should first', async () => {
-    expect(container).toBeTruthy();
-
-    // const nav = document.querySelector<HTMLButtonElement>('nav');
-    // console.log('nav', nav);
-
-    // expect(nav).toBeTruthy();
-    // if(nav) {}
-    // const viewNav = await within(nav!).findByTestId("view-navigate");
-    // viewNav.click();
-
-    // const routeSocket = document.querySelector<HTMLDivElement>('#route-socket');
-    // const viewPage = await within(routeSocket!).findByTestId('view-page');
-
-    // expect(viewPage).toContain('THIS IS THE VIEW PAGE');
+test('should set correct initial starting point [0, 0]', () => {
+    expect(true).toBeTruthy();
 })
